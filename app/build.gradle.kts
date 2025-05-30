0f48d86b5bac6a6406e965a94e5ba6773f1e0e50
Gustavo Brenner <gusynched@gmail.com>
2025-05-30 20:49:57 -0300

    Home screen quase completa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.0-beta01")
     implementation("com.google.firebase:firebase-database")
+    implementation ("com.google.android.material:material:1.1.0-alpha08")
 
 
 }
